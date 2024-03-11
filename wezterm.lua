d830b120e1d0575e7ecf76ef7a161eddf0f6c9cb
Petyo Ivanov <underlog@gmail.com>
2024-03-11 20:53:24 +0200

    fix wezterm padding

--- wezterm.lua
+++ wezterm.lua
@@ -5,9 +5,15 @@ local config = wezterm.config_builder()
 config.enable_scroll_bar = false
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", { weight = "Light", assume_emoji_presentation = true })
 config.line_height = 1.2
-config.font_size = 16.0
+config.font_size = 14.0
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 wezterm.on("toggle-opacity", function(window)
 	local overrides = window:get_config_overrides() or {}
