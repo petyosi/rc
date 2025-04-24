a3905043c4d311956325477b89d7c96d8c31f4ea
Petyo Ivanov <underlog@gmail.com>
2025-04-24 19:08:41 +0300

    Ghostty

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 ln -sf "$PWD"/.tmux/.tmux.conf ~/.tmux.conf
 ln -sf "$PWD"/.tmux.conf.local ~/.tmux.conf.local
 tic -x ./tmux.terminfo
-tic -x -o ~/.terminfo ./wezterm.terminfo
 
 ln -sf "$PWD"/zshrc ~/.zshrc
 ln -sf "$PWD"/inputrc ~/.inputrc
 touch ~/.hushlogin
 
 ln -sf "$PWD"/gitconfig ~/.gitconfig
-ln -sf "$PWD"/alacritty.toml ~/.alacritty.toml
+mkdir -p ~/.config/ghostty
+ln -sf "$PWD"/ghostty-config ~/.config/ghostty/config
